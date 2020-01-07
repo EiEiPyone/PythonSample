e079f4fc6699776e3f095201cadd9f027608d953
EiEiPyone <misseieipyone@gmail.com>
2020-01-07 15:43:22 +0630

    List

---
+++ List.py
@@ -0,0 +1,45 @@
+#List
+
+
+
+
+
+
+
+
+
+
+
+x = 'Hello World'
+x[3]
+x[-5]
+x[-6]
+x[0]
+
+
+word[3:5]
+
+
+
+
+
+
+world = 'Programming'
+P     r  o  g  r  a  m  m  i  n  g
+0     1  2  3  4  5  6  7  8  9  10
+-11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 
+
+
+
+
+
+cube1 = [10, 20, 30, 45, 50]
+cube2 = [1, 2, 3, 4, 5]
+cube3 = cube1 + cube2
+cubeA = [ 'A', 'B', 'C', 'D', 'E']
+cube4 = cube1 + cubeA
+http://docs.python.org/3/tutorial/datastructurers.html
+5.1.3. List Comprehensiions
+5.2  The del Statement
+
+https://github.com/brainseeddeveloper/YTUpython
