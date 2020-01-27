5e277c600f83e24afa642c9b03237a2ea16686b2
EiEiPyone <misseieipyone@gmail.com>
2020-01-27 15:18:51 +0630

    fibonacci

---
+++ fibonacci(import)/fib.py
@@ -0,0 +1,48 @@
+# fib.py
+
+import fibonacci
+
+# f = fibonacci.fib
+# f(200)
+
+# fibonacci.fib2(200)
+# fibonacci.name()
+# fibonacci.hello()
+# fibonacci.fib(10000)
+# --------------
+
+from fibonacci import fib, fib2, dog
+
+f = fib(100)
+print(f)
+dog()
+
+# f = fib2(500)
+# print(f)
+
+# ------------------
+
+# from fibonacci import *
+
+# fib(500)
+# print(fib2(500))
+
+# ---------------
+
+# import fibonacci as fi
+
+# fi.fib(500)
+
+# -------------
+
+# from fibonacci import fib as fibonacci
+# fibonacci(500)
+
+# # https://docs.python.org/3/tutorial/modules.html#packages-in-multiple-directories
+
+# >>> Student (Import Folder)
+
+
+# fi.fib(500)
+
+# -------------------
